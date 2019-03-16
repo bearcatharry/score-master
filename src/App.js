d5be9b911262279753d013230409143d402e7774
duw <duw@umich.edu>
2019-03-16 15:31:32 -0400

    fixing schedule disply

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ class App extends Component {
     chrome.storage.onChanged.addListener((changes, area) => {
       if (area === "sync" && "basketballList" in changes) {
         teamnames = changes.basketballList.newValue;
-        var favTeam = this.state.team;
+        var favTeam = teamnames[0];
         if (this.state.team === "") {
           favTeam = teamnames[0]
         }
@@ -60,11 +60,12 @@ class App extends Component {
           teams: teamnames,
           team: favTeam
           })
-          this.getGames(this.state.teams);
 
         }
       }
       console.log('the updated teams are: ',teamnames);
+      this.getGames(teamnames[0]);
+
     });
 
     
@@ -78,82 +79,119 @@ class App extends Component {
 
     let pastDates = createPastDates(getTodayDate());
     let futureDates = createFutureDates(getTodayDate());
-    const pastGame = [];
-    const futureGames = [];
+    var pastGame = [];
+    var futureGames = [];
+    var allGames = [];
 
-    // Find most recent PAST game
-    for (var i = 0; i < pastDates.length; i++) {
-      let activeDate = pastDates[i].estString;
-      let url;
+    if (team !== "") {
+      // Find most recent PAST game
+      for (var i = 0; i < pastDates.length; i++) {
+        let activeDate = pastDates[i].estString;
+        let url;
 
-      // If it's July... (note that January is 0)
-      if (currentMonth === 6) {
-        // Summer League API URL
-        url =
-          "http://site.api.espn.com/apis/site/v2/sports/basketball/nba-summer-league/scoreboard?lang=en&calendartype=blacklist&limit=100&league=nba-summer-las-vegas&dates=" +
-          activeDate;
+        // If it's July... (note that January is 0)
+        if (currentMonth === 6) {
+          // Summer League API URL
+          url =
+            "http://site.api.espn.com/apis/site/v2/sports/basketball/nba-summer-league/scoreboard?lang=en&calendartype=blacklist&limit=100&league=nba-summer-las-vegas&dates=" +
+            activeDate;
 
-      // If it's not July
-      } else {
-        // NBA API URL.
-        url =
-          "http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard?lang=en&calendartype=blacklist&limit=100&dates=" +
-          activeDate;
-      }
-      
-      let res = await fetch(url);
-      let json = await res.json();
-
-      const tmpGames = json.events;
-      for (var j = 0; j < tmpGames.length; j++) {
-        let gameName = tmpGames[j].shortName;
-        if (gameName.includes(team)) {
-          pastGame.push(tmpGames[j]);
-          break;
+        // If it's not July
+        } else {
+          // NBA API URL.
+          url =
+            "http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard?lang=en&calendartype=blacklist&limit=100&dates=" +
+            activeDate;
         }
+        
+        let res = await fetch(url);
+        let json = await res.json();
+
+        const tmpGames = json.events;
+        for (var j = 0; j < tmpGames.length; j++) {
+          let gameName = tmpGames[j].shortName;
+          if (gameName.includes(team)) {
+            pastGame.push(tmpGames[j]);
+            break;
+          }
+        }
+        if (pastGame.length === 1) {
+          break;
+        }   
       }
-      if (pastGame.length === 1) {
-        break;
-      }   
+
+      // Find NEXT Games
+      for (i = 0; i < futureDates.length; i++) {
+        let activeDate = futureDates[i].estString;
+        let url;
+        // If it's July... (note that January is 0)
+        if (currentMonth === 6) {
+          // Summer League API URL
+          url =
+            "http://site.api.espn.com/apis/site/v2/sports/basketball/nba-summer-league/scoreboard?lang=en&calendartype=blacklist&limit=100&league=nba-summer-las-vegas&dates=" +
+            activeDate;
+
+        // If it's not July
+        } else {
+          // NBA API URL.
+          url =
+            "http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard?lang=en&calendartype=blacklist&limit=100&dates=" +
+            activeDate;
+        }
+        let res = await fetch(url);
+        let json = await res.json();
+
+        const tmpGames = json.events;
+        for (j = 0; j < tmpGames.length; j++) {
+          let gameName = tmpGames[j].shortName;
+          if (gameName.includes(team)) {
+            futureGames.push(tmpGames[j]);
+            break;
+          }
+        }
+
+        if (futureGames.length === 2) {
+          break;
+        } 
+      }
+      allGames = pastGame.concat(futureGames);
+
     }
+    else { 
+      // find three games, no team has been selected
+      for (i = 0; i < futureDates.length; i++) {
+        let activeDate = futureDates[i].estString;
+        let url;
+        // If it's July... (note that January is 0)
+        if (currentMonth === 6) {
+          // Summer League API URL
+          url =
+            "http://site.api.espn.com/apis/site/v2/sports/basketball/nba-summer-league/scoreboard?lang=en&calendartype=blacklist&limit=100&league=nba-summer-las-vegas&dates=" +
+            activeDate;
 
-    // Find NEXT Games
-    for (i = 0; i < futureDates.length; i++) {
-      let activeDate = futureDates[i].estString;
-      let url;
-      // If it's July... (note that January is 0)
-      if (currentMonth === 6) {
-        // Summer League API URL
-        url =
-          "http://site.api.espn.com/apis/site/v2/sports/basketball/nba-summer-league/scoreboard?lang=en&calendartype=blacklist&limit=100&league=nba-summer-las-vegas&dates=" +
-          activeDate;
+        // If it's not July
+        } else {
+          // NBA API URL.
+          url =
+            "http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard?lang=en&calendartype=blacklist&limit=100&dates=" +
+            activeDate;
+        }
+        let res = await fetch(url);
+        let json = await res.json();
 
-      // If it's not July
-      } else {
-        // NBA API URL.
-        url =
-          "http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard?lang=en&calendartype=blacklist&limit=100&dates=" +
-          activeDate;
-      }
-      let res = await fetch(url);
-      let json = await res.json();
-
-      const tmpGames = json.events;
-      for (j = 0; j < tmpGames.length; j++) {
-        let gameName = tmpGames[j].shortName;
-        if (gameName.includes(team)) {
+        const tmpGames = json.events;
+        for (j = 0; j < 3; j++) {
           futureGames.push(tmpGames[j]);
-          break;
         }
+
+        if (futureGames.length === 3) {
+          break;
+        } 
       }
+      allGames = futureGames;
 
-      if (futureGames.length === 2) {
-        break;
-      } 
     }
-
-
-    var allGames = pastGame.concat(futureGames);
+    console.log(team, allGames);
     if ((allGames.length) > 0) {
       // Search this team's game
       // Update state.
@@ -208,7 +246,7 @@ class App extends Component {
         this.setState({
           team: teams[0]
         });
-        console.log(this.state);
+        // console.log(this.state);
 
         // console.log(result);
 
